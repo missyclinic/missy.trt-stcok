9a54bd5dbd09dec9c6b368a6a117e6b478b67d72
missyclinic <missyclinic.acc@gmail.com>
2025-07-14 17:14:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,80 @@ therapist_sheet = spreadsheet.worksheet("therapist_list")
 stock_list_sheet = spreadsheet.worksheet("stock_list")
 usage_sheet = spreadsheet.worksheet("treatment_usage")
 
-TREATMENTS_ALL = treatment_sheet.col_values(1)[1:]
+TREATMENTS_ALL = [
+    "TRT-TURBO 5G",
+    "TRT-Super Shape",
+    "TRT-Super Block",
+    "TRT-Super Firm",
+    "TRT-RELAX",
+    "TRT-Reset Body Skin",
+    "TRT-Gold 24K Body",
+    "TRT-Revive (BODY)",
+    "TRT-Revive- Reduce Booster",
+    "TRT-Revive - Immune System Booster",
+    "TRT-Body Scrub",
+    "TRT-Body Mask",
+    "TRT-Back Acne Treatment (สิวหลัง)",
+    "TRT-Breast Massage 15min",
+    "TRT-Breast Massage & Machine(M9)",
+    "TRT-Slim XS",
+    "TRT-Slim FITS",
+    "TRT-Slim Burm",
+    "TRT-Migraine Relief",
+    "TRT-Super Slim - 30min",
+    "TRT-Super Slim - 15min",
+    "TRT-Super Scrub (เฉพาะจุด)",
+    "TRT-Reset (เช็ดเฉพาะรักแร้)",
+    "TRT-Balance (A,B)",
+    "TRT-Revive (HAIR)",
+    "เจลกายภาพ (3P)",
+    "เจลกายภาพ (5P)",
+    "TRT-กกน",
+    "TRT Boxer",
+    "TRT-หมวก",
+    "TRT-ยาชา",
+    "TRT - Shining skin -Botox",
+    "TRT - Shining skin -Collagen",
+    "TRT - Shining skin -Acne",
+    "TRT - Shining skin -Glow",
+    "TRT - Shining skin Pro - 2P",
+    "TRT-Perfect mask -Charcoal",
+    "TRT-Perfact mask -Bulgarian Rose",
+    "TRT-Perfect mask -Anti-Aging",
+    "TRT-Perfect mask -Vit-C",
+    "TRT-Eye action",
+    "TRT-Missy SkinCell",
+    "TRT-Face Lift  5D",
+    "TRT-Revive Face",
+    "TRT-Golden Aura",
+    "TRT-24K Facial Scrub",
+    "TRT-OXY SKIN CELL",
+    "TRT-ชุดทำความสะอาด+บำรุง",
+    "TRT-Milky ทำความสะอาด",
+    "TRT-Water Micellar ทำความสะอาดก่อนกดสิว",
+    "Magnet SkinPro - Peel exfoliator",
+    "Magnet SkinPro - Wish Care",
+    "Magnet SkinPro - BTX",
+    "Magnet SkinPro - Brightening",
+    "Magnet SkinPro - Neo Energy",
+    "Magnet SkinPro -Collagen",
+    "Magnet SkinPro -Hyaluronic",
+    "Magnet SkinPro - Repairing",
+    "TRT-whitamin C",
+    "TRT-GEL กำจัดขน 3P",
+    "TRT-GEL กำจัดขน 1P",
+    "TRT-GEL 9D",
+    "TRT-Missy Vaginal Repair Cream",
+    "TRT-MISSY MADE+",
+    "TCL - IV AURA-WINK-WHITE ตัวยา",
+    "TCL - IV AURA 100ml. ตัวยา",
+    "TCL-IV AURA 20ml PRO ตัวยา",
+    "TCL-IV FAT BUNRNER ตัวยา",
+    "TRT-MESO AURA 1ML",
+    "แล็ปยาชาหน้ากาก",
+    "มีดโกน 1อัน",
+    "บังทรง"
+]
 THERAPISTS = therapist_sheet.col_values(1)[1:]
 BRANCHES = ["เซ็นทรัลระยอง", "แพชชั่นระยอง", "พระราม2"]
 EQUIPMENTS = ["TRT-หมวก", "TRT-Milky ทำความสะอาด", "TRT-ยาชา", "TRT-กกน", "TRT-ชุดทำความสะอาด+บำรุง", "แล็ปยาชาหน้ากาก"]
@@ -57,7 +130,7 @@ TREATMENT_CHANNEL_ID = 1394115507883606026
     ใช้_หมวก="ใช้ TRT-หมวก หรือไม่",
     ใช้_กกน="ใช้ TRT-กกน หรือไม่",
     ใช้_ชุดทำความสะอาด="ใช้ TRT-ชุดทำความสะอาด+บำรุง หรือไม่",
-    ใช้_milky="ใช้ TRT-Milky ทำความสะอาด หรือไม่",
+    ใช้_Milky="ใช้ TRT-Milky ทำความสะอาด หรือไม่",
     ใช้_ยาชา="ใช้ TRT-ยาชา หรือไม่",
     ใช้_แล็ปยาชา="ใช้ แล็ปยาชาหน้ากาก หรือไม่"
 )
@@ -90,7 +163,7 @@ async def ส่งtrt(interaction: discord.Interaction,
     ใช้_หมวก: bool = False,
     ใช้_กกน: bool = False,
     ใช้_ชุดทำความสะอาด: bool = False,
-    ใช้_milky: bool = False,
+    ใช้_Milky: bool = False,
     ใช้_ยาชา: bool = False,
     ใช้_แล็ปยาชา: bool = False
 ):
@@ -112,7 +185,7 @@ async def ส่งtrt(interaction: discord.Interaction,
         equipment_used.append("TRT-กกน")
     if ใช้_ชุดทำความสะอาด:
         equipment_used.append("TRT-ชุดทำความสะอาด+บำรุง")
-    if ใช้_milky:
+    if ใช้_Milky:
         equipment_used.append("TRT-Milky ทำความสะอาด")
     if ใช้_ยาชา:
         equipment_used.append("TRT-ยาชา")
