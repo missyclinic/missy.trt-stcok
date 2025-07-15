457837fc4240a4186e8376e0dac5ed18fc311158
missyclinic <missyclinic.acc@gmail.com>
2025-07-15 09:20:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,17 +20,17 @@ TREATMENTS_SPLIT_1 = ["TRT-TURBO 5G", "TRT-Super Shape", "TRT-Super Block", "TRT
 
 TREATMENTS_SPLIT_2 = ["เจลกายภาพ (5P)", "TRT-กกน", "TRT Boxer", "TRT-หมวก", "TRT-ยาชา", "TRT - Shining skin -Botox", "TRT - Shining skin -Collagen", "TRT - Shining skin -Acne", "TRT - Shining skin -Glow", "TRT - Shining skin Pro - 2P", "TRT-Perfect mask -Charcoal", "TRT-Perfact mask -Bulgarian Rose", "TRT-Perfect mask -Anti-Aging", "TRT-Perfect mask -Vit-C", "TRT-Eye action", "TRT-Missy SkinCell", "TRT-Face Lift  5D", "TRT-Revive Face", "TRT-Golden Aura", "TRT-24K Facial Scrub", "TRT-OXY SKIN CELL", "TRT-ชุดทำความสะอาด+บำรุง", "TRT-Milky ทำความสะอาด", "TRT-Water Micellar ทำความสะอาดก่อนกดสิว", "Magnet SkinPro - Peel exfoliator"]
 
-TREATMENTS_SPLIT_3 = ["TRT-Revive (HAIR)","Magnet SkinPro - Wish Care", "Magnet SkinPro - BTX", "Magnet SkinPro - Brightening", "Magnet SkinPro - Neo Energy", "Magnet SkinPro -Collagen", "Magnet SkinPro -Hyaluronic", "Magnet SkinPro - Repairing", "TRT-whitamin C", "TRT-GEL กำจัดขน 3P", "TRT-GEL กำจัดขน 1P", "TRT-GEL 9D", "TRT-Missy Vaginal Repair Cream", "TRT-MISSY MADE+", "TCL - IV AURA-WINK-WHITE ตัวยา", "TCL - IV AURA 100ml. ตัวยา", "TCL-IV AURA 20ml PRO ตัวยา", "TCL-IV FAT BUNRNER ตัวยา", "TRT-MESO AURA 1ML", "แล็ปยาชาหน้ากาก", "มีดโกน 1อัน", "บังทรง"]
+TREATMENTS_SPLIT_3 = ["TRT-Revive (HAIR)", "Magnet SkinPro - Wish Care", "Magnet SkinPro - BTX", "Magnet SkinPro - Brightening", "Magnet SkinPro - Neo Energy", "Magnet SkinPro -Collagen", "Magnet SkinPro -Hyaluronic", "Magnet SkinPro - Repairing", "TRT-whitamin C", "TRT-GEL กำจัดขน 3P", "TRT-GEL กำจัดขน 1P", "TRT-GEL 9D", "TRT-Missy Vaginal Repair Cream", "TRT-MISSY MADE+", "TCL - IV AURA-WINK-WHITE ตัวยา", "TCL - IV AURA 100ml. ตัวยา", "TCL-IV AURA 20ml PRO ตัวยา", "TCL-IV FAT BUNRNER ตัวยา", "TRT-MESO AURA 1ML", "แล็ปยาชาหน้ากาก", "มีดโกน 1อัน", "บังทรง"]
 
 THERAPISTS = ["วิ PRY", "รัก PRY", "ปาร์ตี้ SM", "คนที่ 4", "คนที่ 5", "คนที่ 6", "คนที่ 7"]
 BRANCHES = ["เซ็นทรัลระยอง", "แพชชั่นระยอง", "พระราม2"]
-EQUIPMENTS = ["TRT-หมวก", "TRT-Milky ทำความสะอาด", "TRT-ยาชา", "TRT-กกน", "TRT-ชุดทำความสะอาด+บำรุง", "แล็ปยาชาหน้ากาก"]
+EQUIPMENTS = ["ไม่ใช้", "TRT-หมวก", "TRT-Milky ทำความสะอาด", "TRT-ยาชา", "TRT-กกน", "TRT-ชุดทำความสะอาด+บำรุง", "แล็ปยาชาหน้ากาก"]
 
 ADMIN_CHANNEL_ID = 1394115416049061918
 ADMIN_ONLY_CHANNEL_ID = 1394133334317203476
 TREATMENT_CHANNEL_ID = 1394115507883606026
 
-@tree.command(name="ส่งtrt", description="บันทึก Treatment พร้อมอุปกรณ์")
+@tree.command(name="ส่งtrt", description="บันทึกทรีตเมนต์พร้อมอุปกรณ์")
 @app_commands.describe(
     สาขา="เลือกสาขา",
     ลูกค้า="ชื่อลูกค้า",
@@ -48,31 +48,15 @@ TREATMENT_CHANNEL_ID = 1394115507883606026
     treatment3_3="ทรีตเมนต์ 3 กลุ่ม 3",
     therapist3="พนักงาน 3"
 )
-@app_commands.choices(
-    สาขา=[app_commands.Choice(name=b, value=b) for b in BRANCHES],
-    ใช้อุปกรณ์=[app_commands.Choice(name=e, value=e) for e in EQUIPMENTS] + [app_commands.Choice(name="ไม่ใช้", value="ไม่ใช้")],
-    treatment1_1=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_1],
-    treatment1_2=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_2],
-    treatment1_3=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_3],
-    treatment2_1=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_1],
-    treatment2_2=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_2],
-    treatment2_3=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_3],
-    treatment3_1=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_1],
-    treatment3_2=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_2],
-    treatment3_3=[app_commands.Choice(name=t, value=t) for t in TREATMENTS_SPLIT_3],
-    therapist1=[app_commands.Choice(name=t, value=t) for t in THERAPISTS],
-    therapist2=[app_commands.Choice(name=t, value=t) for t in THERAPISTS],
-    therapist3=[app_commands.Choice(name=t, value=t) for t in THERAPISTS]
-)
 async def ส่งtrt(interaction: discord.Interaction, สาขา: str, ลูกค้า: str, ใช้อุปกรณ์: str,
-                 treatment1_1: Optional[str], treatment1_2: Optional[str], treatment1_3: Optional[str], therapist1: Optional[str],
-                 treatment2_1: Optional[str], treatment2_2: Optional[str], treatment2_3: Optional[str], therapist2: Optional[str],
-                 treatment3_1: Optional[str], treatment3_2: Optional[str], treatment3_3: Optional[str], therapist3: Optional[str]):
+                 treatment1_1: Optional[str] = None, treatment1_2: Optional[str] = None, treatment1_3: Optional[str] = None, therapist1: Optional[str] = None,
+                 treatment2_1: Optional[str] = None, treatment2_2: Optional[str] = None, treatment2_3: Optional[str] = None, therapist2: Optional[str] = None,
+                 treatment3_1: Optional[str] = None, treatment3_2: Optional[str] = None, treatment3_3: Optional[str] = None, therapist3: Optional[str] = None):
 
     await interaction.response.defer(thinking=True)
     today_date = datetime.now().strftime("%Y-%m-%d")
     group_id = f"{today_date.replace('-', '')}-{str(uuid.uuid4())[:6]}"
-    msg = f"✅ บันทึก Treatment สำหรับ\nชื่อลูกค้า {ลูกค้า}\nทำที่ : {สาขา}\nGroup ID: {group_id}\nรายการTRT\n"
+    msg = f"✅ บันทึกทรีตเมนต์สำหรับ\nชื่อลูกค้า {ลูกค้า}\nทำที่ : {สาขา}\nGroup ID: {group_id}\nรายการ TRT\n"
     treatments = [
         (treatment1_1 or treatment1_2 or treatment1_3, therapist1),
         (treatment2_1 or treatment2_2 or treatment2_3, therapist2),
